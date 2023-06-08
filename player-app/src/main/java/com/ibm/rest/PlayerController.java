6e1ca2a566e50dc1937ee63fa7055849c16f665d
Zubair Shaikh <zubairify@gmail.com>
2023-06-08 10:37:37 +0530

    Player Microservice update

--- player-app/src/main/java/com/ibm/rest/PlayerController.java
+++ player-app/src/main/java/com/ibm/rest/PlayerController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.entity.Player;
@@ -12,23 +13,24 @@ import com.ibm.service.PlayerService;
 import com.ibm.vo.ResponseTemplate;
 
 @RestController
+@RequestMapping("/player")
 public class PlayerController {
 
 	@Autowired
 	private PlayerService service;
 	
-	@PostMapping(value = "/player", consumes = "application/json")
+	@PostMapping(value = "/", consumes = "application/json")
 	public String addPlayer(@RequestBody Player player) {
 		service.save(player);
 		return "Player added.";
 	}
 	
-	@GetMapping(value = "/player/{id}", produces = "application/json")
+	@GetMapping(value = "/{id}", produces = "application/json")
 	public Player getPlayer(@PathVariable int id) {
 		return service.fetch(id);
 	}
 	
-	@GetMapping(value = "/playerWithTeam/{id}", produces = "application/json")
+	@GetMapping(value = "/withTeam/{id}", produces = "application/json")
 	public ResponseTemplate getPlayerWithTeam(@PathVariable int id) {
 		return service.fetchPlayerWithTeam(id);
 	}
